e4ae3bc908e3443408c6000933da0ff9de2c1f30
hansj.melby <hansj.melby@gmail.com>
2019-08-06 11:46:32 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+port = int(os.getenv("PORT", 9099))
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+# Test
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
